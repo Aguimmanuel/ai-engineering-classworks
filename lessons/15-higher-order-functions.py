7c3712d442b8bce7e6c32fdcafef27099d5c348d
Aguimmanuel <aguimmanuel7@gmail.com>
2026-08-06 11:53:20 +0100

    completed lesson 15 higher order functions challenges

--- lessons/15-higher-order-functions.py
+++ lessons/15-higher-order-functions.py
@@ -9,7 +9,7 @@ Lesson 15: Higher Order Functions Script
 # print(updated_prices)
 menu_prices = [3500.0, 4200.0, 5000.0]
 updated_prices = map(lambda price: price + 500.0, menu_prices)
-print(list(updated_prices))
+print(list(updated_prices))     #modified the print statement to cast the map object to a list so that it prints the actual values instead of a memory address
 
 
 
@@ -27,35 +27,53 @@ chalkboard_menu = [
     {"name": "Latte", "price": 4500.0},
     {"name": "Mocha", "price": 5000.0}
 ]
-budget_options = list(filter(lambda x: x["price"] <= 4000.0, chalkboard_menu))
+budget_options = list(filter(lambda x: x["price"] <= 4000.0, chalkboard_menu)) #corrected the order of the parameters in the filter function, placing the lambda function first and the list second
 print(budget_options)
 
 
 # Challenge 3
 # Instruction: Provide the missing target parameter check on the sorting invocation so that Python knows to look inside the dictionaries and organize them cleanly by price
+# beverage_catalog = [
+#     {"name": "Mocha", "price": 5000.0},
+#     {"name": "Espresso", "price": 3500.0},
+#     {"name": "Latte", "price": 4500.0}
+# ]
+# sorted_catalog = sorted(beverage_catalog)
+# print(sorted_catalog)
 beverage_catalog = [
     {"name": "Mocha", "price": 5000.0},
     {"name": "Espresso", "price": 3500.0},
     {"name": "Latte", "price": 4500.0}
 ]
-sorted_catalog = sorted(beverage_catalog)
+sorted_catalog = sorted(beverage_catalog, key=lambda x: x["price"]) #added the key parameter to the sorted function, specifying that the sorting should be based on the "price" key in each dictionary
 print(sorted_catalog)
 
 
 # Challenge 4
 # Instruction: Clean up the syntax by refactoring this functional map-lambda chain into a sleek, pythonic List Comprehension that produces identical mathematical data outcomes
+# base_counts = [10, 20, 30]
+# scaled_counts = list(map(lambda x: x * 3, base_counts))
+# print(scaled_counts)
+ 
 base_counts = [10, 20, 30]
-scaled_counts = list(map(lambda x: x * 3, base_counts))
+scaled_counts = [x * 3 for x in base_counts] #refactored the map-lambda chain into a list comprehension
 print(scaled_counts)
 
 
 # Challenge 5
 # Instruction: Correct the broken map lambda so that it correctly mutates the price metric while passing back the rest of the object layout, preventing it from dropping names and converting to raw numbers
+# raw_inventory = [
+#     {"name": "Cup", "count": 10},
+#     {"name": "Spoon", "count": 5}
+# ]
+# updated_inventory = list(map(lambda item: item["count"] + 2, raw_inventory))
+# print(updated_inventory)
+
 raw_inventory = [
     {"name": "Cup", "count": 10},
     {"name": "Spoon", "count": 5}
 ]
-updated_inventory = list(map(lambda item: item["count"] + 2, raw_inventory))
+updated_inventory = list(map(lambda item: {"name": item["name"], "count": item["count"] + 2}, raw_inventory))
 print(updated_inventory)
 
 
